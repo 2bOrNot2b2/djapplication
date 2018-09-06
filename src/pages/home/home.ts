62773ea75fce5e71954f6e8d222003bfa2ce5013
karabomaimane <maimane23@hotmail.com>
2018-09-06 10:59:03 +0200

    fixing some ref errors

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-<<<<<<< HEAD
 import { RegisterPage } from '../register/register';
-=======
-<<<<<<< HEAD
 import { LoginPage } from '../login/login';
-=======
 import { CategoriesPage } from '../categories/categories';
->>>>>>> 65aff7b8bcb0f7d7e0d101b85d404e4feb6bee64
->>>>>>> b571b215cb4811d64060786570adab6c50f72294
 
 @Component({
   selector: 'page-home',
@@ -20,20 +14,15 @@ export class HomePage {
 
   }
 
-<<<<<<< HEAD
   Register(){
     this.navCtrl.push(RegisterPage);
   }
-=======
-<<<<<<< HEAD
+
   login(){
     this.navCtrl.push(LoginPage);
   }
 
-=======
   nextPage(){
     this.navCtrl.push(CategoriesPage);
   }
->>>>>>> 65aff7b8bcb0f7d7e0d101b85d404e4feb6bee64
->>>>>>> b571b215cb4811d64060786570adab6c50f72294
 }
