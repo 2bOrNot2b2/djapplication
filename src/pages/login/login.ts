4220fec4aa0124d62e6b8faf1fb98406d71b7de4
Your Name <you@example.com>
2018-09-06 10:49:33 +0200

    kulani commit

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -15,7 +15,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+   var  kjsagddhgkjsagkjhsahgdhdkjsad;
   }
 
   ionViewDidLoad() {
