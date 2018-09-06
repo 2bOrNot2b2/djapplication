91b6f5ffc8274ee02a6d017064bfdb7e275fca6c
Your Name <you@example.com>
2018-09-06 11:04:29 +0200

    kulani fixed

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -19,12 +19,10 @@ export class LoginPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
 
-<<<<<<< HEAD
 
-=======
    var  kjsagddhgkjsagkjhsahgdhdkjsad;
    var change;
->>>>>>> b2c0f1ecc2408f89df4a5e225b30cb044ed40e37
+
   }
 
   ionViewDidLoad() {
